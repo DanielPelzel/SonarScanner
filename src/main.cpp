b37ff9c8d2286f35478eba4fd9334dc7acf74072
Daniel Pelzel <pelzel.daniel@gmail.com>
2026-03-23 21:26:37 +0100

    first try ultrasonic Sensor (not working)

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,41 @@
 #include <Arduino.h>
 
 
-void setup() {
+//Variablen definieren
+float seconds;
+float distance_m;
+float distance_cm;
 
+
+void setup() {
+    pinMode(13, OUTPUT);
+    pinMode(12, INPUT);
+    Serial.begin(9600);
 }
 
 void loop() {
+
+    // Sicherstellen, dass der Pin vorher LOW ist
+    digitalWrite(13, LOW);
+    delayMicroseconds(2);
+
+    //Sending Input signal (10 microseconds)
+
+    digitalWrite(13, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(13, LOW);
+
+    //ultrasonic wave time
+
+    seconds = pulseIn(12, HIGH) * 1e-6;
+
+
+    //time to distance calc.
+
+    distance_m = (seconds * 340.0 /2.0);
+    distance_cm = distance_m * 100;
+    Serial.println(distance_cm);
+
+
 
 }
